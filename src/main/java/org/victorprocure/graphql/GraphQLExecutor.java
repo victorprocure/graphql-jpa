d592e4288b30c79b52c978c5af28839a54237f7b
Victor Procure <victor@victorprocure.co>
2017-07-02 03:50:34 -0600

    Added dependency resolver

--- src/main/java/org/victorprocure/graphql/GraphQLExecutor.java
+++ src/main/java/org/victorprocure/graphql/GraphQLExecutor.java
@@ -83,4 +83,8 @@ public class GraphQLExecutor {
         return graphQL.execute(query, context, arguments);
     }
 
+    public void setDependencyResolver(IDependencyResolver dependencyResolver) {
+        this.dependencyResolver = dependencyResolver;
+    }
+
 }

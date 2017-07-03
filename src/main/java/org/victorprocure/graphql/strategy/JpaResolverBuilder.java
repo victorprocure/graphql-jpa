a87e8120a7a3b307b8b0ddff93daeef3f9a7ec79
Victor Procure <victor@victorprocure.co>
2017-07-02 20:24:46 -0600

    version bump

--- src/main/java/org/victorprocure/graphql/strategy/JpaResolverBuilder.java
+++ src/main/java/org/victorprocure/graphql/strategy/JpaResolverBuilder.java
@@ -1,11 +1,18 @@
 package org.victorprocure.graphql.strategy;
 
+import graphql.execution.batched.Batched;
 import io.leangen.graphql.metadata.Resolver;
-import io.leangen.graphql.metadata.strategy.query.AnnotatedArgumentBuilder;
-import io.leangen.graphql.metadata.strategy.query.BeanOperationNameGenerator;
-import io.leangen.graphql.metadata.strategy.query.FilteredResolverBuilder;
-import io.leangen.graphql.metadata.strategy.query.PublicResolverBuilder;
+import io.leangen.graphql.metadata.execution.MethodInvoker;
+import io.leangen.graphql.metadata.execution.SingletonMethodInvoker;
+import io.leangen.graphql.metadata.strategy.query.*;
 import io.leangen.graphql.util.ClassUtils;
+import graphql.execution.batched.Batched;
+import io.leangen.graphql.metadata.Resolver;
+import io.leangen.graphql.metadata.execution.MethodInvoker;
+import io.leangen.graphql.metadata.execution.SingletonMethodInvoker;
+import io.leangen.graphql.util.ClassUtils;
+import io.leangen.graphql.util.Utils;
+import io.leangen.graphql.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.victorprocure.graphql.configuration.IGraphQLJpaConfiguration;
@@ -17,66 +24,36 @@ import javax.persistence.criteria.Root;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Created by victo on 7/2/2017.
  */
-public class JpaResolverBuilder extends FilteredResolverBuilder {
+public class JpaResolverBuilder extends PublicResolverBuilder {
     private static Logger log = LoggerFactory.getLogger(JpaResolverBuilder.class);
     private EntityManager entityManager;
 
     public JpaResolverBuilder(EntityManager entityManager) {
-        this.operationNameGenerator = new BeanOperationNameGenerator();
+        super(null);
+        this.operationNameGenerator = new MethodOperationNameGenerator();
         this.argumentExtractor = new AnnotatedArgumentBuilder();
         this.entityManager = entityManager;
     }
 
-    @Override
-    public Collection<Resolver> buildQueryResolvers(Object querySourceBean, AnnotatedType beanType) {
-
-        return this.buildQueryResolvers(querySourceBean, beanType, this.getFilters());
-    }
 
     @Override
-    public Collection<Resolver> buildMutationResolvers(Object querySourceBean, AnnotatedType beanType) {
-
-        return this.buildMutationResolvers(querySourceBean, beanType, this.getFilters());
+    protected boolean isQuery(Method method) {
+        return super.isQuery(method) && ClassUtils.isGetter(method);
     }
 
-    private Collection<Resolver> buildQueryResolvers(Object querySourceBean, AnnotatedType beanType, List<Predicate<Member>> filters) {
-        log.debug(
-                new StringBuilder()
-                .append("querySource: ")
-                        .append(querySourceBean)
-                        .append(" annotated: ")
-                        .append(beanType)
-                        .append(" filters: ")
-                        .append(filters)
-                        .toString());
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        Type t = beanType.getType();
-        Class<?> clazz = (Class<?>)t;
-        CriteriaQuery<Object> query = cb.createQuery((Class) t);
-        Root root = query.from(clazz);
-
-    return Collections.emptyList();
+    @Override
+    protected boolean isMutation(Method method) {
+        return ClassUtils.isSetter(method);
     }
 
-    private Collection<Resolver> buildMutationResolvers(Object querySourceBean, AnnotatedType beanType, List<Predicate<Member>> filters) {
-        log.debug(
-                new StringBuilder()
-                        .append("querySource: ")
-                        .append(querySourceBean)
-                        .append(" annotated: ")
-                        .append(beanType)
-                        .append(" filters: ")
-                        .append(filters)
-                        .toString());
-    return Collections.emptyList();
-    }
 }
